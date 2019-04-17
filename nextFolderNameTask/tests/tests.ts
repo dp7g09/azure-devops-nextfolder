35895f7ccb3d51e2329bc0f1a5868c62b8230d1c
Dushyant Priyadarshee <dushyant.priyadarshee@sharp-ls.eu>
2019-04-17 19:03:48 +0100

    Test - Should work for folder with sequentially named folders.

--- nextFolderNameTask/tests/tests.ts
+++ nextFolderNameTask/tests/tests.ts
@@ -10,4 +10,11 @@ describe('Test NextNameGenerator', function () {
         assert.equal(nextName, 'RC1', 'Next name should have been RC1 for empty folder.');
         done();
     });
+
+    it('Should work for folder with sequentially named folders', function(done: Mocha.Done) {
+        this.timeout(20000);
+        var nextName = getDirItems('.//tests//TestData//SequentialNames', 'RC');
+        assert.equal(nextName, 'RC3', 'Next name should have been RC3.');
+        done();
+    });
 });
